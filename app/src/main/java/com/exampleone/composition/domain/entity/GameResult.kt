511d7f8becf07905f52781e1e5a8992594a7e295
Николай <kolyas56atm@gmail.com>
2022-03-22 01:27:16 +0300

    Add Navigation by FragmentManager

--- app/src/main/java/com/exampleone/composition/domain/entity/GameResult.kt
+++ app/src/main/java/com/exampleone/composition/domain/entity/GameResult.kt
@@ -1,10 +1,9 @@
 package com.exampleone.composition.domain.entity
+import java.io.Serializable
 
 data class GameResult(
-
     val winner: Boolean,
     val countOfRightAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-
-)
+): Serializable
--- app/src/main/java/com/exampleone/composition/domain/entity/Level.kt
+++ app/src/main/java/com/exampleone/composition/domain/entity/Level.kt
@@ -1,5 +1,6 @@
 package com.exampleone.composition.domain.entity
 
+
 enum class Level {
 
     TEST, EASY, NORMAL, HARD
